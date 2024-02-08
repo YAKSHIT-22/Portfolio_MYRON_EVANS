344e9924180cabb333bdd285ac9d60f883723992
Kushagra <kush.54321gpt@gmail.com>
2024-02-08 17:40:27 +0530

    Change: naming of Classes and styling of vertical timeline

--- frontend/src/components/VerticalTime.jsx
+++ frontend/src/components/VerticalTime.jsx
@@ -1,38 +1,77 @@
-import React from 'react';
-import { VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import LineIcon from './LineIcon';
-const VerticalTime = ({ date, title, subtitle, description, id }) => {
-    return (
+import PropTypes from 'prop-types'
+import { VerticalTimelineElement } from 'react-vertical-timeline-component'
 
-        <VerticalTimelineElement
-            key={id}
-            className={`vertical-timeline-element--work w-90 -mb-[10rem]`}
-            contentStyle={{ background: '#2C2B2B', color: '#fff', boxShadow: "none" }}
-            contentArrowStyle={{ display: 'none' }}
-            iconStyle={{ border: "0px", boxShadow: "none", width: "34px", transform: `${(id % 2 === 0) ? "rotate(180deg) translateX(-1.44rem)" : ""}` }}
-            // fix the paddigns for resposnive design margin and svg --remove padding
-            icon={<LineIcon />}
-            position={(id % 2) ? "left" : "right"}
+import 'react-vertical-timeline-component/style.min.css'
+import LineIcon from './LineIcon'
+
+const VerticalTime = ({ date, title1, title2, subtitle, description, id }) => {
+  return (
+    <VerticalTimelineElement
+      key={id}
+      className={`vertical-timeline-element--work -mb-[10rem]`}
+      contentStyle={{
+        background: '#2C2B2B',
+        color: '#fff',
+        boxShadow: 'none',
+        marginLeft: id % 2 ? '-3.5rem' : '0',
+        marginRight: id % 2 === 0 ? '-3.5rem' : 0,
+        width: '51%',
+      }}
+      contentArrowStyle={{ display: 'none' }}
+      iconStyle={{
+        border: '0px',
+        boxShadow: 'none',
+        width: '34px',
+        transform: `${id % 2 === 0 ? 'rotate(180deg) translateX(-1.44rem)' : ''}`,
+      }}
+      // fix the paddinGs for responsive design margin and svg --remove padding
+      icon={<LineIcon />}
+      position={id % 2 ? 'left' : 'right'}
+    >
+      <div className="items-left relative mb-1 space-x-2"></div>
+      <h3 className="vertical-timeline-element-subtitle font-mulish ml-1 text-button font-medium text-secondary">
+        {date}
+      </h3>
+      <h4 className="vertical-timeline-element-title mt-3 min-w-80 bg-gradient bg-clip-text text-content-heading text-transparent">
+        <p
+          style={{
+            marginTop: 0,
+            fontWeight: 800,
+            fontSize: '1.625rem',
+            lineHeight: '2rem',
+          }}
         >
-
-            <div className="reltive items-left space-x-2 mb-1">
-
-
-
-
-            </div>
-            <h3 className="vertical-timeline-element-subtitle font-medium text-secondary"> {date}</h3>
-            <h4 className="vertical-timeline-element-title text-[#8D611D] w-80 text-3xl font-[400] ">{title}</h4>
-            <h5 className='text-primary'> {subtitle}</h5>
-            <p className='text-primary text-lg font-light leading-7'>
-                {description}                </p>
-        </VerticalTimelineElement>
-
-
-
-
-    )
+          {title1}
+        </p>
+        <p
+          style={{
+            marginTop: 0,
+            fontWeight: 800,
+            fontSize: '1.625rem',
+            lineHeight: '2rem',
+          }}
+        >
+          {title2}
+        </p>
+      </h4>
+      <h5 className="te mt-3 text-[20px] text-primary"> {subtitle}</h5>
+      <p
+        className="mt-3 text-body leading-7 text-secondary"
+        style={{ fontWeight: 300, marginTop: '1rem' }}
+      >
+        {description}
+      </p>
+    </VerticalTimelineElement>
+  )
 }
 
 export default VerticalTime
+
+VerticalTime.propTypes = {
+  date: PropTypes.string.isRequired, // Add this line to validate the 'date' prop
+  title1: PropTypes.string.isRequired,
+  title2: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+}
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,32 +1,20 @@
 import { Link } from 'react-router-dom'
-import Lottie from 'react-lottie'
+import 'react-vertical-timeline-component/style.min.css'
+import { VerticalTimeline } from 'react-vertical-timeline-component'
 
 import myPurposeIcon from '../assets/Animations/icons animation/My Purpose/LottieJSON.json'
 import myAmbitionIcon from '../assets/Animations/icons animation/My Ambition/LottieJSON.json'
 import faceAnimation from '../assets/Animations/Face Animation/Preview.gif'
 import imageBg from '../assets/svgs/img-bg.svg'
 import section2Img from '../assets/images/Home Page/Section 2 img.png'
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
 
-// import Section4 from '../assets/images/Home Page/Section4.jpeg'
 import { Experience } from '../assets/data'
-import whatDrivesMe from '../assets/Animations/Title Animation/Home Page Desktop/What Drives me_/What Drives me.json'
-import verticalTime from '../components/VerticalTime'
+
 import VerticalTime from '../components/VerticalTime'
 import SkillsCard from '../components/SkillsCard'
-function HomeScreen() {
-  function defaultOptions(animationData) {
-    return {
-      loop: false,
-      autoplay: true,
-      animationData: animationData,
-      rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice',
-      },
-    }
-  }
+import AnimatedIcon from '../components/AnimatedIcon'
 
+function HomeScreen() {
   return (
     <div>
       {/* Section 1 ~ Face Animation */}
@@ -56,7 +44,7 @@ function HomeScreen() {
             <h3 className="bg-gradient bg-clip-text text-content-heading font-bold text-transparent">
               What drives me
             </h3>
-            <h2 className="text-section-heading font-bold">
+            <h2 className="text-section-heading font-extrabold">
               <p>Crafting stories through</p>
               <p>Development and Design</p>
             </h2>
@@ -64,15 +52,7 @@ function HomeScreen() {
 
           <div className="w-full">
             <div className="mt-7">
-              <Lottie
-                title="My Ambition"
-                options={defaultOptions(myAmbitionIcon)}
-                height={52}
-                width={80}
-                renderer={''}
-                isClickToPauseDisabled
-                style={{ margin: 0, cursor: 'unset' }}
-              />
+              <AnimatedIcon iconData={myAmbitionIcon} height={42} width={70} />
               <h2 className="mt-4 text-content-heading font-extrabold">
                 My Ambition
               </h2>
@@ -87,15 +67,7 @@ function HomeScreen() {
               </p>
             </div>
             <div className="mt-7">
-              <Lottie
-                title="My Purpose"
-                options={defaultOptions(myPurposeIcon)}
-                height={80}
-                width={80}
-                renderer={''}
-                isClickToPauseDisabled
-                style={{ margin: 0, cursor: 'unset' }}
-              />
+              <AnimatedIcon iconData={myPurposeIcon} height={62} width={80} />
               <h2 className="text-content-heading">My Purpose</h2>
               <p className="mt-4 text-body text-secondary">
                 My purpose is to impact as many people as possible in a positive
@@ -118,31 +90,52 @@ function HomeScreen() {
 
       {/* Section 3 ~ Vertical Timeline*/}
       <section>
-        <div className='mt7'>
-          <VerticalTimeline >
-            {Experience && Experience.map((n) => (
-
-              <VerticalTime date={n.date} subtitle={n.subtitle} title={n.title} description={n.description} id={n.id} key={n.id} />
-            ))}
-          </VerticalTimeline >
+        <div className="mt7">
+          <VerticalTimeline>
+            {Experience &&
+              Experience.map((n) => (
+                <VerticalTime
+                  key={n.id}
+                  id={n.id}
+                  date={n.date}
+                  title1={n.title1}
+                  title2={n.title2}
+                  subtitle={n.subtitle}
+                  description={n.description}
+                />
+              ))}
+          </VerticalTimeline>
         </div>
-
-
       </section>
 
       {/* Section 4 ~ Share Div */}
-
       <section>
-        <div className="mt-7 w-full flex justify-center items-center relative">
+        <div className="relative mt-7 flex w-full items-center justify-center">
           {/* Background image div */}
-          <div className='absolute left-auto right-auto w-5/6 inset-0 rounded-[2rem] h-[39rem] bg-no-repeat bg-center bg-cover opacity-40 duration-500 ease-in-out hover:opacity-20' style={{ backgroundImage: "url('/Section4.jpeg')" }} ></div>
+          <div
+            className="absolute inset-0 left-auto right-auto h-[35rem] w-5/6 rounded-3xl bg-cover bg-center bg-no-repeat opacity-40 duration-500 ease-in-out hover:opacity-20"
+            style={{ backgroundImage: "url('/Section4.jpeg')" }}
+          ></div>
 
           {/* Text content div  cerate it responsive for md */}
 
-          <div className='relative z-10 h-[39rem] flex flex-col items-center justify-center mt-6rem ml-7rem text-white'>
-            <h1 className='w-[42rem] text-5xl text-center'>Share my website and story with someone it can help</h1>
-            <span className='w-[42rem] mt-[4rem]'>We all have struggles and failures in life, please share my website with anyone you believe my story can help. If you are inspired by my story and would like to support me, please contact me for a project, refer a client to me using my affiliate referral program, or visit my store, some of the proceeds from my store will go towards helping my freelance career grow and the rest will go to Bridging Tech and Tech Kids Unlimited. </span>
-            <Link to="/my-work" className="hover:shadow-custom text-center w-fit h-15 mt-7 rounded-full bg-gradient px-10 py-3 transition-all">
+          <div className="pl-18 relative z-10 flex h-full flex-col items-center justify-center py-14 text-white">
+            <h1 className="w-[42rem] text-center text-section-heading">
+              Share my website and story with someone it can help
+            </h1>
+            <span className="mt-16 w-[42rem] text-body">
+              We all have struggles and failures in life, please share my
+              website with anyone you believe my story can help. If you are
+              inspired by my story and would like to support me, please contact
+              me for a project, refer a client to me using my affiliate referral
+              program, or visit my store, some of the proceeds from my store
+              will go towards helping my freelance career grow and the rest will
+              go to Bridging Tech and Tech Kids Unlimited.{' '}
+            </span>
+            <Link
+              to="/my-work"
+              className="h-15 font-rubik mt-7 w-fit rounded-full bg-gradient px-10 py-3 text-center font-medium transition-all hover:shadow-custom hover:shadow-golden"
+            >
               SHARE NOW
             </Link>
           </div>
@@ -150,21 +143,28 @@ function HomeScreen() {
       </section>
 
       {/* Section 5 ~ Services */}
-      <section className='mt-10'>
-        <div className='flex flex-col jsutify-center items-center'>
-          <h1 className='text-5xl'> My <span className='bg-gradient px-3'>Skills</span></h1>
-          <p className='text-lg font-sans w-5/6 mt-10'>Throughout the years I have fostered skills that help me create interesting and unique projects.  Check them out and see if I have a skill that could benefit you and your customers.</p>
-          <div className='flex flex-col justify-items-start items-start w-5/6 mt-10 '>
-            <h3 className='text-lg w-100 inline'>Full Stack Developer with:</h3>
-            <div className='flex w-full'>
+      <section className="mt-10"></section>
+
+      {/* Section 6 ~ Skills */}
+      <section>
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-5xl">
+            {' '}
+            My <span className="bg-gradient px-3">Skills</span>
+          </h1>
+          <p className="mt-10 w-5/6 font-sans text-lg">
+            Throughout the years I have fostered skills that help me create
+            interesting and unique projects. Check them out and see if I have a
+            skill that could benefit you and your customers.
+          </p>
+          <div className="mt-10 flex w-5/6 flex-col items-start justify-items-start ">
+            <h3 className="w-100 inline text-lg">Full Stack Developer with:</h3>
+            <div className="flex w-full">
               <SkillsCard title="FLutter/Dart" percent={45} icon="icon" />
             </div>
           </div>
         </div>
       </section>
-
-      {/* Section 6 ~ Skills */}
-      <section></section>
 
       {/* Section 7 ~ Core Values */}
       <section></section>
