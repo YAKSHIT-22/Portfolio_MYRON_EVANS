449fc4e8adbaa90ce44698b98c9742b5fbd15522
Tanshul <74649098+tans-hul@users.noreply.github.com>
2024-02-11 00:00:09 +0530

    blog screen some

--- frontend/src/screens/BlogScreen.jsx
+++ frontend/src/screens/BlogScreen.jsx
@@ -1,5 +1,15 @@
 function BlogScreen() {
-  return <div>BlogScreen</div>
+  return <section>
+    <div>
+      Go Back Button
+    </div>
+    <div className="flex flex-col">
+      <div className={`mt-12 flex flex-col-reverse  w-[72rem] h-[44rem]  bg-no-repeat rounded-[2rem]`} style={{ backgroundImage: `url('${img}')` }}>
+
+
+      </div>
+    </div>
+  </section>
 }
 
 export default BlogScreen
